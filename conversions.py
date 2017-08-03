860bdf010d27047e56f33b58d237389c5bef63ac
Mikayla Konst <mikayla.konst@gmail.com>
2017-08-03 08:46:49 -0700

    added more comments about padding

--- conversions.py
+++ conversions.py
@@ -9,17 +9,20 @@ def a_to_c(asc):
     return chr(asc)
 
 # ASCII to Binary
-# Example: 97 --> '1100001'
+# Example: 97 --> '01100001'
 def a_to_b(a):
     binary = bin(a)[2:] # chop off leading two characters, which are '0b'
     # pad with leading 0's to reach 8 characters
     zeros_to_add = 8 - len(binary)
     for i in range(zeros_to_add):
         binary = "0" + binary
+    # bin(a) returns 0b1100001
+    # bin(a)[2:] returns 1100001
+    # after padding, it is 01100001
     return binary
 
 # Binary to ASCII
-# Example: '1100001' --> 97
+# Example: '01100001' --> 97
 def b_to_a(b):
     return int(b, 2) # the 2 is for base 2, i.e. binary
 
